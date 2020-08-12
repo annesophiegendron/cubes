7f000987a53aaa7632068afd62129da5e04816b9
annesophiegendron <annesophie.gendron@gmail.com>
2020-08-12 15:28:14 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.scss'
+
+import { Canvas } from "react-three-fiber"
+
+
+function App() {
+  return (
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" />
+        
+        </mesh> 
+
+      </Canvas>
+    </>
+  )
+}
+
+export default App
